ca10dbdb79a2093080a67690643586055269cb26
sorrentmutie <salvatoresorrentino801@gmail.com>
2016-10-13 10:14:25 +0200

    Creazione classe Studenti

---
+++ Teleta02/Progetto/Progetto.Models/Studenti/Studente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto.Models.Studenti
+{
+    public class Studente
+    {
+        public string  FullName { get; set; }
+        public string FirstName { get; set; }
+
+        private string sesso;
+        public string Sex {
+            get
+            {
+                return sesso;
+            }
+            set
+            {
+                sesso = value;
+            }
+        }
+
+        public string  MiddleName { get; set; }
+        public string  LastName { get; set; }
+
+        public string PhotoFileName { get; set; }
+        public Double GradePointAverage { get; set; }
+
+    }
+}
