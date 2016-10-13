b9d55bd6a3e05d211a4a69c2e575df379268e2ba
sorrentmutie <salvatoresorrentino801@gmail.com>
2016-10-13 17:35:51 +0200

    Voglio andare a casa

---
+++ Teleta02/Progetto/Progetto.TestSerializzazioneXML/Program.cs
@@ -0,0 +1,24 @@
+﻿using Progetto.Models.Studenti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto.TestSerializzazioneXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Studenti = new List<Studente>();
+            Studenti.Add(new Studente { FirstName = "Vincenzo", LastName = "Iacobelli" });
+            Studenti.Add(new Studente { FirstName = "Giovanni", LastName = "Sassanelli" });
+            Studenti.Add(new Studente { FirstName = "Salvatore", LastName = "Sorrentino" });
+            
+            var salvatore = Studenti.Where(x => x.FirstName == "Salvatore").FirstOrDefault();
+            Console.WriteLine(salvatore.FirstName);
+            Console.ReadKey(); 
+        }
+    }
+}
